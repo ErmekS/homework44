22119cdfe9dc9ab66f310a40ff1957182918c65a
ErmekSolpuev <ermeksee27@gmail.com>
2022-06-26 16:22:12 +0600

    перенес логику игры

--- homework44/urls.py
+++ homework44/urls.py
@@ -16,6 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from webapp.views import get_stat, index_view
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index_view),
+    path('stat/', get_stat),
+
 ]
